6857ce3170907dd780df1c2b536e7a554973304d
freshton98 <55086586+freshton98@users.noreply.github.com>
2019-09-24 22:21:10 +0530

    Add files via upload

---
+++ assign2.py
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Freshton Jebastin
+#
+# Created:     24/09/2019
+# Copyright:   (c) Freshton Jebastin 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+fileName = input("Enter the file name : ")
+inputFile= open(fileName,'r')
+text = inputFile.read()
+
+
+sentences=text.count('.')+text.count('?')+\
+           text.count(':')+text.count(';')+\
+           text.count('!')
+
+words=len(text.split())
+
+
+syllables = 0
+for word in text.split():
+    for vowel in ['a','e','i','o','u']:
+        syllables+=word.count(vowel)
+    for ending in ['es','ed','e']:
+        if word.endswith(ending):
+            syllables-=1
+        if word.endswith('le'):
+            syllables+=1
+
+index = 206.835-1.015*(words/float(sentences))-\
+        84.6*(syllables/words)
+level =int(round(0.39*(words/float(sentences))+11.8*\
+                 (syllables/float(words))-15.59))
+
+
+print ("THE FLESCH INDEX IS",index)
+print ("THE GRADE LEVEL EQAIVALENT IS",level)
+print ("SENTENCES",sentences)
+print ("WORDS",words)
+print ("SYLLABLES",syllables)
